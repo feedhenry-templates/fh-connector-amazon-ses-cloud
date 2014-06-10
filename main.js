0c04240bf81495b8f1c9fd931590d81d17a6a710
Jason Madigan <jason.madigan@feedhenry.com>
2014-06-10 17:13:42 +0100

    Move

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
+var AWS = require('aws-sdk');
+var accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+var secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+AWS.config.update({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey
+});
+AWS.config.update({
+  region: 'eu-west-1'
+});
+
 /*
   Returns a list containing all verified email addresses
  */
 exports.ses = function(params, cb) {
-  var AWS = require('aws-sdk');
-  var accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-  var secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-  AWS.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  });
-  AWS.config.update({
-    region: 'eu-west-1'
-  });
-
-
   var ses = new AWS.SES();
   ses.listVerifiedEmailAddresses({}, function(err, data) {
     if (err) {
